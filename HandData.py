549055cb4aa0fe9c2167e4771a5a243ab66837b9
damachun <c.limjiahan@gmail.com>
2021-08-19 23:09:30 +0800

    hand data processing

--- HandData.py
+++ HandData.py
@@ -1,38 +1,39 @@
 from enum import Enum
 import numpy as np
+from typing import Tuple
 
-class HandIndices(int, Enum):
-    THUMB  = 2  # 02 to 04
-    INDEX  = 6  # 06 to 08
-    MIDDLE = 10 # 10 to 12
-    RING   = 14 # 14 to 16
-    PINKY  = 18 # 18 to 20
+class HandIndices(Tuple[int, int], Enum):
+    THUMB  = (0, 2  ) # 02 to 04
+    INDEX  = (1, 6  ) # 06 to 08
+    MIDDLE = (1, 10 ) # 10 to 12
+    RING   = (1, 14 ) # 14 to 16
+    PINKY  = (1, 18 ) # 18 to 20
 
 class HandData:
     def __init__(self, threshold : float = 10.0):
         # raw positions -> can retrieve via hand indices enum
         self.__position = { 
-            HandIndices.THUMB  : np.zeros((3, 2), dtype = float),
-            HandIndices.INDEX  : np.zeros((3, 2), dtype = float),
-            HandIndices.MIDDLE : np.zeros((3, 2), dtype = float),
-            HandIndices.RING   : np.zeros((3, 2), dtype = float),
-            HandIndices.PINKY  : np.zeros((3, 2), dtype = float) }
+            HandIndices.THUMB  : np.zeros((3, 1), dtype = float),
+            HandIndices.INDEX  : np.zeros((3, 1), dtype = float),
+            HandIndices.MIDDLE : np.zeros((3, 1), dtype = float),
+            HandIndices.RING   : np.zeros((3, 1), dtype = float),
+            HandIndices.PINKY  : np.zeros((3, 1), dtype = float) }
         #self.__offsets   = np.zeros((21, 2), dtype = float)
 
         # reference data
         self.__prev_position = { 
-            HandIndices.THUMB  : np.zeros((3, 2), dtype = float),
-            HandIndices.INDEX  : np.zeros((3, 2), dtype = float),
-            HandIndices.MIDDLE : np.zeros((3, 2), dtype = float),
-            HandIndices.RING   : np.zeros((3, 2), dtype = float),
-            HandIndices.PINKY  : np.zeros((3, 2), dtype = float) }
+            HandIndices.THUMB  : np.zeros((3, 1), dtype = float),
+            HandIndices.INDEX  : np.zeros((3, 1), dtype = float),
+            HandIndices.MIDDLE : np.zeros((3, 1), dtype = float),
+            HandIndices.RING   : np.zeros((3, 1), dtype = float),
+            HandIndices.PINKY  : np.zeros((3, 1), dtype = float) }
         #self.__reference_offsets = np.zeros((21, 2), dtype = float)
         self.__active = { 
-            HandIndices.THUMB  : 0,
-            HandIndices.INDEX  : 0,
-            HandIndices.MIDDLE : 0,
-            HandIndices.RING   : 0,
-            HandIndices.PINKY  : 0 }
+            HandIndices.THUMB  : False,
+            HandIndices.INDEX  : False,
+            HandIndices.MIDDLE : False,
+            HandIndices.RING   : False,
+            HandIndices.PINKY  : False }
 
         self.__threshold = threshold
 
@@ -45,24 +46,34 @@ class HandData:
         temparray = temparray.astype("float64")
 
         for hand_idx in HandIndices:
-            num_idx = hand_idx.value
+            coord_idx = hand_idx.value[0]
+            num_idx = hand_idx.value[1]
             for add in range(3):
-                if abs(temparray[num_idx + add][0] - self.__position[hand_idx][add][0]) > self.__threshold:
-                    self.__prev_position[hand_idx][add][0], self.__position[hand_idx][add][0] = self.__position[hand_idx][add][0], temparray[num_idx + add][0]
-                if abs(temparray[num_idx + add][1] - self.__position[hand_idx][add][1]) > self.__threshold:
-                    self.__prev_position[hand_idx][add][1], self.__position[hand_idx][add][1] = self.__position[hand_idx][add][1], temparray[num_idx + add][1] 
+                if abs(temparray[num_idx + add][coord_idx] - self.__position[hand_idx][add]) > self.__threshold:
+                    self.__prev_position[hand_idx][add], self.__position[hand_idx][add] = self.__position[hand_idx][add], temparray[num_idx + add][coord_idx]
+
+    def activity(self):
+        for hand_index, position_set in self.__position.items():
+            self.__active[hand_index] = (position_set[1] < position_set[0]) and (position_set[2] < position_set[0])
 
     # catered specifically for HandDetect hand_details
     def update(self, all_positions_data):
         x_pos = [sublist[2] for sublist in all_positions_data]
         y_pos = [sublist[3] for sublist in all_positions_data]
         self.position(x_pos, y_pos)
+        self.activity()
 
     def finger_active(self, finger_index = HandIndices.THUMB):
-        return self.__active[finger_index] == 1
+        return self.__active[finger_index]
 
-    def num_of_fingers_active(self) -> int:
-        return sum(self.__active.values())
+    def finger_active_data(self) -> int:
+        return ("Hand Activity:\n\t" +
+            "Thumb: "  + str(self.__active[HandIndices.THUMB])  + "\n\t" +
+            "Index: "  + str(self.__active[HandIndices.INDEX])  + "\n\t" +
+            "Middle: " + str(self.__active[HandIndices.MIDDLE]) + "\n\t" +
+            "Ring: "   + str(self.__active[HandIndices.RING])   + "\n\t" +
+            "Pinky: "  + str(self.__active[HandIndices.PINKY]))
+            #sum(self.__active.values()) 
 
     def __repr__(self):
         hand_activity_str = ("Hand Activity:\n\t" +
@@ -72,13 +83,13 @@ class HandData:
             "Ring: "   + str(self.__active[HandIndices.RING])   + "\n\t" +
             "Pinky: "  + str(self.__active[HandIndices.PINKY]))
         previous_pos_str = "Previous Positions:\n\t" + "\n\t".join(
-            ("%-18s + %d: (%5.2f, %5.2f)" % (
-            str(hand_index), index, prev_xy[0], prev_xy[1]))
+            ("%-18s + %d: (%5.2f)" % (
+            str(hand_index), index, prev_xy))
             for hand_index in HandIndices
             for index, prev_xy in enumerate(self.__prev_position[hand_index]))
         pos_str = "Previous Positions:\n\t" + "\n\t".join(
-            ("%-18s + %d: (%5.2f, %5.2f)" % (
-            str(hand_index), index, curr_xy[0], curr_xy[1]))
+            ("%-18s + %d: (%5.2f)" % (
+            str(hand_index), index, curr_xy))
             for hand_index in HandIndices
             for index, curr_xy in enumerate(self.__prev_position[hand_index]))
 
@@ -112,4 +123,4 @@ if __name__ == "__main__":
         if key == 27: break
         elif key == ord("q"): print(repr(hand_data))
         #elif key == ord("w"): hand_data.recalibrate_offset(hand_detector.get_details)
-        elif key == ord("e"): print(hand_data.num_of_fingers_active())
+        elif key == ord("e"): print(hand_data.finger_active_data())
